5e12099eb748e677e840059e6a32eb772779f1a0
Nguyen Anh Nhut <nguyenanhnhut0101.99@gmail.com>
2020-09-10 05:19:43 +0700

    update lan 7

--- js/main.js
+++ js/main.js
@@ -21,24 +21,10 @@ disclosureHeadings.addEventListener("click", (e) => {
 });
 
 // Carousel
-
+var prevSlide = document.getElementById("prevSlide");
+var nextSlide = document.getElementById("nextSlide");
 var imgSlide = document.getElementsByClassName("carousel__item");
 var numSlide = 0;
-
-const prevSlide = () => {
-   numSlide--;
-   if (numSlide < 0) {
-      numSlide = 2;
-   }
-   slideShow(numSlide);
-}
-const nextSlide = () => {
-   numSlide++;
-   if (numSlide > 2) {
-      numSlide = 0;
-   }
-   slideShow(numSlide);
-}
 
 const slideShow = (n) => {
    for (let i = 0; i < imgSlide.length; i++) {
@@ -47,46 +33,108 @@ const slideShow = (n) => {
    imgSlide[n].style.display = "block";
 }
 slideShow(0);
-
+prevSlide.addEventListener("click", () => {
+   numSlide--;
+   if (numSlide < 0) {
+      numSlide = 2;
+   }
+   slideShow(numSlide);
+})
+nextSlide.addEventListener("click", () => {
+   numSlide++;
+   if (numSlide > 2) {
+      numSlide = 0;
+   }
+   slideShow(numSlide);
+})
 
 // Modal Search 
 var searchButton = document.getElementById("searchButton");
 var searchModal = document.getElementById("searchPages");
 var searchClose = document.querySelector(".search-close");
 
-searchButton.addEventListener("click",function(){
+searchButton.addEventListener("click", function () {
    searchModal.classList.add("search-active");
 })
 
-searchClose.addEventListener("click",function(){
+searchClose.addEventListener("click", function () {
    searchModal.classList.remove("search-active");
 
 })
 
-
-
-$(window).on('scroll', function () {
-   if ($(window).scrollTop()) {
-      $('#fordHeader').addClass('sticky-ford');
+const forHeader = document.getElementById("fordHeader");
+window.addEventListener("scroll", () => {
+   if (window.pageYOffset > 20) {
+      forHeader.classList.add("sticky-ford");
    }
    else {
-      $('#fordHeader').removeClass('sticky-ford');
+      setTimeout(() => {
+         forHeader.classList.remove("sticky-ford");
+      }, 300);
    }
-});
+})
 // back to top
 var backToTop = document.querySelector(".backToTop");
-window.addEventListener("scroll",() => {
-   if(window.pageYOffset > 250){
+window.addEventListener("scroll", () => {
+   if (window.pageYOffset > 300) {
       backToTop.classList.add("backToTop-active");
    }
-   else{
+   else {
       setTimeout(() => {
          backToTop.classList.remove("backToTop-active");
-      },200);
-     
-   }
-})
+      }, 200);
 
-backToTop.addEventListener("click",() => {
-   window.scrollTo(0,0);
-})
+   }
+});
+
+backToTop.addEventListener("click", () => {
+   window.scrollTo(0, 0);
+});
+
+// Form Login and Register
+
+var loginBtn = document.getElementById("login");
+var registerBtn = document.getElementById("register");
+var btn = document.getElementById("btn");
+
+function register() {
+   loginBtn.style.left = "-400px";
+   registerBtn.style.left = "50px";
+   btn.style.left = "110px";
+}
+function login() {
+   loginBtn.style.left = "50px";
+   registerBtn.style.left = "450px";
+   btn.style.left = "0";
+}
+
+
+
+// Login System
+
+var arrUser = [
+   {
+      userName: "nhut",
+      passWord: "123"
+   },
+   {
+      userName: "anhnhut",
+      passWord: "123"
+   }
+]
+
+function getInfo(){
+   var userName = document.getElementById("userName").value;
+   var passWord = document.getElementById("passWord").value;
+   var loginModal = document.querySelector(".loginContainer");
+   for (let i = 0; i < arrUser.length; i++) {
+      if (userName == arrUser[i].userName && passWord == arrUser[i].passWord) {
+         loginModal.classList.add("outLogin");
+      }
+   }
+};
+
+function guestMode(){
+   var loginModal = document.querySelector(".loginContainer");
+   loginModal.classList.add("outLogin");
+}
